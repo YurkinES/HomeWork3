8648256594847e7ffc6b1a035a5772c7f52f864b
YurkinES <6eJlomop@inbox.ru>
2023-02-15 01:28:49 +0300

    Third stage

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        //Task #1
+        byte numberPlanetEarth = 3;
+       short depthMarianaTrench = 10920;
+       int populationChina = 1411750000;
+       long distanceEarthToSun = 149600000L;
+       float pi = 3.14F;
+       double allPi = 3.1415926535;
+       System.out.println("Значение переменной numberPlanetEarth с типом byte равно " + numberPlanetEarth);
+       System.out.println("Значение переменной depthMarianaTrench с типом short равно " + depthMarianaTrench);
+       System.out.println("Значение переменной populationChina с типом int равно " + populationChina);
+       System.out.println("Значение переменной distanceEarthToSun с типом long равно " + distanceEarthToSun);
+       System.out.println("Значение переменной pi с типом float равно " + pi);
+       System.out.println("Значение переменной allPi с типом double равно " + allPi);
+       //Task #2
+        float a = 27.12f;
+        long b = 987678965549L;
+        float c = 2.786f;
+        short d = 569;
+        short e = -159;
+        short f = 27897;
+        byte g = 67;
+        //Task #3
+        byte firstClass = 23;
+        byte secondClass = 27;
+        byte thirdClass = 30;
+        short allPaperSheets = 480;
+        int allStudents = firstClass + secondClass + thirdClass;
+        int paperSheetsForOneStudent = allPaperSheets / allStudents;
+        System.out.println("На одного ученика приходится по " + paperSheetsForOneStudent + " листов бумаги");
+        //Task #4
+
+
+
+    }
+}
